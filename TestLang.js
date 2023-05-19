21194389cd74ae4d9f6b8bee61d954545ba1ebb0
dubbernaut <134111840+dubbernaut@users.noreply.github.com>
2023-05-19 16:55:43 -0700

    Add files via upload

---
+++ TestLang.js
@@ -0,0 +1,72 @@
+chatLangCallback({
+  "APPLICATION_TYPE": "Chat",
+  "S_PAGE_TITLE": "Chat123",
+  "S_PROVIDER": "<span class='provider-text'>Provided by <span class='provider-five'>Five</span><span class='provider-nine'>9</span></span>",
+  "S_NO_NAME": "Me"
+  "S_INITIALIZATION_PAGE": {
+    "S_CONTENT": "Initialization..."
+  },
+  "S_INFORMATION_PAGE": {
+    "S_PROFILE_LABEL": "Test123",
+    "S_NAME_LABEL": "Name123",
+    "S_EMAIL_LABEL": "Email address123",
+    "S_QUESTION_LABEL": "Question123",
+    "S_START_BUTTON": "Start to chat 123"
+  },
+  "S_CONNECTING_PAGE": {
+    "S_CONTENT": "You are being connected to an agent."
+  },
+  "S_CONVERSATION_PAGE": {
+    "S_SEND_BUTTON": "Send",
+    "S_EXIT_CONFERENCE": "Agent {0} left the conference.",
+    "S_TRANSFER_GROUP": "You have been transferred to group {0}.",
+    "S_AGENT_END": "The agent ended the chat.",
+    "S_CUSTOMER_END": "The customer ended the chat.",
+    "S_THANK_YOU_END": "Thank you.",
+    "S_START_NEW": "Start a new chat",
+    "S_TYPING": "Typing...",
+    "S_VIDEO_SESSION_REQUEST_RECEIVED": "Video and/or Audio session request received", 
+    "S_VIDEO_SESSION_CANCELED": "Video and/or audio session canceled",
+    "S_VIDEO_SESSION_REJECTED": "Video and/or audio session rejected",
+    "S_VIDEO_SESSION_ACCEPTED": "Video and/or audio session accepted",
+    "S_VIDEO_ACTIVATED": "Video and/or audio activated",
+    "S_VIDEO_TERMINATED": "Video and/or audio terminated",
+    "S_INPUT_MESSAGE_PLACEHOLDER": "Type here..."
+  },
+  "S_ERROR_PAGE": {
+    "S_START_OVER": "Start Over",
+    "S_SEND_EMAIL": "Send Email"
+  },
+  "S_TERMINATE_POPUP": {
+    "S_TERMINATE_BUTTON": "End",
+    "S_TERMINATE_CONFIRM": "Are you sure that you want to end the chat?"
+  },
+  "S_ERRORS": {
+    "S_CONNECTING": "We are experiencing connection problems. Try again later.", 
+    "S_WEB_SOCKET": "The current version of your browser is not supported.<br /><br />Upgrade to a modern browser.", 
+    "S_LOCAL_STORAGE": "The current version of your browser is not supported.<br /><br />Upgrade to a modern browser.",
+    "S_LOCAL_STORAGE_SAFARI": "The current version of your browser is not supported (or you are using a Private Window in Safari).<br /><br />Upgrade to a modern browser.",
+    "S_BEFORE_UNLOAD": "If you reload this page, you will exit the current chat session.", 
+    "S_NO_SERVICE": "No agents are currently available. Try again later or send an email.", 
+    "S_NETWORK_LOST": "The network connection was lost. Check your network connection or try again later." 
+  },
+  
+  "S_MESSAGES": {
+    "S_TO": "Agent {0} is now chatting with you.",
+    "S_FROM": "Agent {0} transferred the chat to {1}",
+    "S_GR_IN_PROGRESS": "We are experiencing network problems. The problem may last up to 10 minutes. Our agent will respond to your message as soon as possible. Thank you for your patience."
+
+ 
+  },
+  "S_END_CHAT": "Chat ended",
+  "S_CANCEL": "Cancel",
+  "S_VALIDATE": {
+    "S_REQUIRED": "Required field",
+    "S_INVALID_EMAIL": "Invalid email address"
+  },
+   "S_VIDEO_POPUP": {
+    "S_ACCEPT": "Accept",
+    "S_REJECT": "Reject",
+    "S_MESSAGE": "Video and/or audio session requested"
+  }
+});
